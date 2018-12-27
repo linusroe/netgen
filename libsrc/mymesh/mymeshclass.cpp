1e3ec47dd29bbf42ea7c15dccd973da1f1a923e6
linus.roepert <linus.roepert@gmail.com>
2018-12-27 23:59:01 +0100

    Added edge and node functionality

--- libsrc/mymesh/mymeshclass.cpp
+++ libsrc/mymesh/mymeshclass.cpp
@@ -1,1 +1,38 @@
 #include "mymeshclass.hpp"
+
+namespace netgen
+{
+MyMesh :: MyMesh(std::size_t dimX,
+                std::size_t dimY,
+                std::size_t dimZ)
+{
+    double ptStepX = dimX ? 1/dimX : 0; 
+    double ptStepY = dimY ? 1/dimY : 0; 
+    double ptStepZ = dimZ ? 1/dimZ : 0; 
+
+    for (std::size_t i = 0; i <= dimX; ++i)
+    {
+        for (std::size_t j = 0; j <= dimY; ++j)
+        {
+            for (std::size_t k = 0; k <= dimZ; ++k)
+            {
+                Node p{i*ptStepX, j*ptStepY, k*ptStepZ};
+                nodes.push_back(p);
+            }
+        }
+    }
+
+    for (Node from : nodes)
+    {
+        for(Node to : nodes)
+        {
+            if(from != to && (from.x - to.x <= ptStepX  && dimX ||
+               from.y - to.y <= ptStepY && dimY || from.z - to.z <= ptStepZ && dimZ))
+                {
+                    Edge e{from, to};
+                    edges.push_back(e);
+                }
+        }
+    }
+}
+}
--- libsrc/mymesh/mymeshclass.hpp
+++ libsrc/mymesh/mymeshclass.hpp
@@ -2,24 +2,56 @@
 #define MYMESHCLASS
 #endif
 
+#include <cstdlib>
 #include <memory>
+#include <vector>
 
 namespace netgen
 {
   class MyMesh
   {
+  public:
+    class Node
+    {
+    public:
+      double x;
+      double y;
+      double z;
+
+      Node(double x_ = 0, double y_ = 0, double z_ = 0) :
+      x{x_}, y{y_}, z{z_} {}
+    };
+    class Edge
+    {
+    public:
+      Node a;
+      Node b;
+      Edge(Node a_, Node b_) :
+      a{a_}, b{b_} {}
+    };
+
+    class Face
+    {
+    public:
+      std::vector<Node> nodes;
+      std::vector<Edge> edges;
+      Face(std::vector<Node> nodes_, std::vector<Edge> edges_) :
+      nodes{nodes_}, edges{edges_} {}
+    };
+
   private:
     int dim;
     int numNodes;
     int numEdges;
     int numFaces;
     int numVolumes;
+    std::vector<Node> nodes;
+    std::vector<Edge> edges;
+    std::vector<Face> faces;
+
 
   public:
-    MyMesh(std::size_t dimX = 0, std::size_t dimY = 0, std::size_t dimZ = 0)
-    : dim{0}
-    {
-    }
+    MyMesh(std::size_t dimX = 0, std::size_t dimY = 0, std::size_t dimZ = 0);
 
     int getDim(){return dim;}
     int getNumNodes(){return numNodes;}
@@ -27,4 +59,14 @@ namespace netgen
     int getNumFaces(){return numFaces;}
     int getNumVolumes(){return numVolumes;}
   };
+
+  bool operator==(const MyMesh::Node& lhs, const MyMesh::Node& rhs)
+  {
+  if(lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z)
+      return true;
+    return false;
+  }
+
+  bool operator!=(const MyMesh::Node& lhs, const MyMesh::Node& rhs) {return !(lhs == rhs);}
+
 }
