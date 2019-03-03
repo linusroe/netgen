a1f51eaa8e1f8e46d85a25369066c3910a15d532
linus.roepert <linus.roepert@gmail.com>
2019-03-03 02:00:53 +0100

    Added test skeleton for Ngx_MyMesh tests

--- libsrc/mymesh/mymeshclasstest.cpp
+++ libsrc/mymesh/mymeshclasstest.cpp
@@ -59,5 +59,37 @@ int main()
 
     std::cout << "\n\n\n---------Testing Interface-------\n\n";
     netgen::Ngx_MyMesh ngsmym(spm);
-    std::cout << ngsmym.GetDimension() << "\n";
+    std::cout << "Testing GetDimension(): " << ngsmym.GetDimension() << "\n";
+    std::cout << "Testing GetNLevels(): " << ngsmym.GetNLevels() << "\n\n";
+
+    std::cout << "Testing GetNElemts(0): " << ngsmym.GetNElements(0) << "\n";
+    std::cout << "Testing GetNElemts(1): " << ngsmym.GetNElements(1) << "\n";
+    std::cout << "Testing GetNElemts(2): " << ngsmym.GetNElements(2) << "\n";
+    std::cout << "Testing GetNElemts(3): " << ngsmym.GetNElements(3) << "\n\n";
+
+    std::cout << "Testing GetNNodes1(): " << ngsmym.GetNNodes1() << "\n";
+    std::cout << "Testing GetNNodes2(): " << ngsmym.GetNNodes1() << "\n";
+
+    std::cout << "Testing GetPoint(): MISSING!!" << "\n\n";
+
+    std::cout << "Testing GetElement0(5): " << "\n";
+    netgen::Ng_Element ngel0{ngsmym.GetElement0(5)};
+    std::cout << "Return type: " << ngel0.type << " Return index: " << ngel0.index << " !!HARDCODED!!\n";
+    std::cout << "Points number: " << ngel0.points.num << "\n";
+    std::cout << "Points array: " << ngel0.points[0] << "\n";
+
+    std::cout << "\n";
+
+    std::cout << "Testing GetMaterialCD0(0): " << ngsmym.GetMaterialCD0(0) << "\n";
+    std::cout << "SAME FOR ALL OTHER\n\n";
+
+    std::cout << "Testing ElementTransformation(): NOT IMPLEMENTED" << "\n\n";
+    std::cout << "Testing MultiElementTransformation(): NOT IMPLEMENTED" << "\n\n";
+
+    std::cout << "Testing GetNode(): ADD TEST" << "\n\n";
+
+    std::cout << "Testing GetNNodes1(): " << ngsmym.GetNNodes1() << "\n";
+    std::cout << "Testing GetNNodes2(): " << ngsmym.GetNNodes2() << "\n";
+
+    std::cout << "SKIP UNIMPLEMENTED REST" << "\n\n";
 }
