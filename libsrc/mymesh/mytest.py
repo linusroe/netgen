d47e008a7d0e4be86e46349ca41c46cd3ef837c3
linus.roepert <linus.roepert@gmail.com>
2019-03-19 23:55:00 +0100

    Reformated test file and added additional tests

--- libsrc/mymesh/mytest.py
+++ libsrc/mymesh/mytest.py
@@ -1,75 +1,47 @@
-#import netgen.gui
 from ngsolve import *
-from netgen.csg import unit_cube
-from ngsolve.meshes import *
-
-mesh = MakeQuadMesh(nx=5,ny=5)
-#asdf
-
-for v in mesh.vertices:
-    print (v, v.point)
-
-# input("step 1 done")
-
-for e in mesh.edges:
-    print (e)# e.elements)
-
-# input("step 2 done")
-
-# for el in mesh.Elements(VOL):
-#     print (el, el.vertices, el.edges)
-
-# input("step 3 done")
-
-# for el in mesh.Elements(BND):
-#     print (el, el.vertices, el.edges)
-
-# input("step 4 done")
-
-
-# V=H1(mesh,order=1)
-# gfu = GridFunction(V)
-# gfu.Set(sin(10*x))
-# integral = Integrate(gfu,mesh)
-
-# Draw(gfu,mesh,"u")
-# print(integral, " =!= ", 0.183129283601153)
-
-
 from netgen.mymesh import *
+
+output = 0
 
 mesh = MyMesh(2,2,2)
 ngxmymesh = Ngx_MyMesh(mesh)
-
-from ngsolve import *
 ngsmesh = Mesh(ngxmymesh)
 
-for v in ngsmesh.vertices:
-    print (v, v.point)
+if output:
+    for v in ngsmesh.vertices:
+        print (v, v.point)
 
-input("step 1 done")
+    input("step 1 done")
 
-for e in ngsmesh.edges:
-    print (e, e.elements)
+    for e in ngsmesh.edges:
+        print (e, e.elements)
 
-input("step 2 done")
+    input("step 2 done")
 
-for el in ngsmesh.Elements(VOL):
-    print (el, el.vertices, el.edges)
+    for el in ngsmesh.Elements(VOL):
+        print (el, el.vertices, el.edges)
 
-input("step 3 done")
+    input("step 3 done")
 
-for el in ngsmesh.Elements(BND):
-    print (el, el.vertices, el.edges)
+    for el in ngsmesh.Elements(BND):
+        print (el, el.vertices, el.edges)
 
-input("step 4 done")
+    input("step 4 done")
 
-print("Mesh works up to H1")
+    print("Mesh works up to H1")
 
 V=H1(ngsmesh,order=1,no_low_order_space=True)
 gfu = GridFunction(V)
-gfu.Set(sin(10*x))
-integral = Integrate(gfu, ngsmesh)
 
+gfu.Set(1) # == 1
+integral = Integrate(gfu, ngsmesh)
+print("Int 1 dxdydz: ", integral, " =!= ", 1)
+
+gfu.Set(x+y+z) # == 1.5
+integral = Integrate(gfu, ngsmesh)
+print("Int x+y+z dxdydz: ", integral, " =!= ", 1.5)
+
+gfu.Set(x*sin(y**2)+z) # == .655
+integral = Integrate(gfu, ngsmesh)
+print("Int x*sin(y^2)+z dxdydz: ", integral, " =!= ", .665)
 # Draw(gfu,mesh,"u")
-print(integral, " =!= ", 0.183129283601153)
