ca4b0af4b7e0ad56b09c04fd2bb7bb963c68d0e3
linus.roepert <linus.roepert@gmail.com>
2019-04-01 23:25:58 +0200

    Added comments and fixed typo

--- libsrc/mymesh/mytest.py
+++ libsrc/mymesh/mytest.py
@@ -1,47 +1,50 @@
 from ngsolve import *
 from netgen.mymesh import *
 
+# Walk through all elements of mesh
 output = 0
 
+# Create 2 x 2 x 2 Mesh
 mesh = MyMesh(2,2,2)
 ngxmymesh = Ngx_MyMesh(mesh)
 ngsmesh = Mesh(ngxmymesh)
 
+# Iterate and print all elements
 if output:
     for v in ngsmesh.vertices:
         print (v, v.point)
 
-    input("step 1 done")
+    input("points done")
 
     for e in ngsmesh.edges:
         print (e, e.elements)
 
-    input("step 2 done")
+    input("edges done")
 
     for el in ngsmesh.Elements(VOL):
         print (el, el.vertices, el.edges)
 
-    input("step 3 done")
+    input("vertices done")
 
     for el in ngsmesh.Elements(BND):
         print (el, el.vertices, el.edges)
 
-    input("step 4 done")
+    input("volumes done")
 
     print("Mesh works up to H1")
 
 V=H1(ngsmesh,order=1,no_low_order_space=True)
 gfu = GridFunction(V)
 
+# Compute various integrals to verify mesh function
 gfu.Set(1) # == 1
 integral = Integrate(gfu, ngsmesh)
-print("Int 1 dxdydz: ", integral, " =!= ", 1)
+print("Int 1 dxdydz: ", integral, " == ", 1)
 
 gfu.Set(x+y+z) # == 1.5
 integral = Integrate(gfu, ngsmesh)
-print("Int x+y+z dxdydz: ", integral, " =!= ", 1.5)
+print("Int x+y+z dxdydz: ", integral, " == ", 1.5)
 
 gfu.Set(x*sin(y**2)+z) # == .655
 integral = Integrate(gfu, ngsmesh)
-print("Int x*sin(y^2)+z dxdydz: ", integral, " =!= ", .665)
-# Draw(gfu,mesh,"u")
+print("Int x*sin(y^2)+z dxdydz: ", integral, " == ", .655)
